edde0988e146fcd5f31e09df27af3c449f55d2a6
Julian Zinn <79761+jzinn@users.noreply.github.com>
2018-05-27 02:50:53 -0700

    lazy: Generalize and remove hard coded list

--- load-lazy-images.js
+++ load-lazy-images.js
@@ -12,22 +12,55 @@
 (function() {
     'use strict';
 
-    load('data-mediaviewer-src'); // https://www.nytimes.com/interactive/2018/05/24/climate/dry-rio-grande.html
-    load('data-native-src'); // https://www.bloomberg.com/news/articles/2018-05-14/drone-pilots-2-000-paydays-drop-90-in-race-to-the-bottom
-    load('data-original'); // https://www.macrumors.com/
-    load('data-src'); // https://www.atlasobscura.com/articles/what-did-pterosaurs-do-with-their-legs
-    load('data-dt-lazy-src'); // https://www.digitaltrends.com/home-theater/how-to-install-kodi-on-an-amazon-fire-tv/
+    images().forEach(update);
 
-    function load(lazy) {
-        images().forEach(update);
+    function images() {
+        return Array.from(document.getElementsByTagName('img'));
+    }
 
-        function images() {
-            return document.querySelectorAll('img[' + lazy + ']');
+    function update(image) {
+        updateIf(pick(attributes(), image.className));
+
+        function updateIf(attribute) {
+            if (!attribute) return;
+            image.src = attribute.value;
+            restyle(image.style, window.getComputedStyle(image));
         }
 
-        function update(image) {
-            image.setAttribute('src', image.getAttribute(lazy));
-            restyle(image.style, window.getComputedStyle(image));
+        function attributes() {
+            return Array.from(image.attributes).filter(isntEqSrc);
+        }
+    }
+
+    function isntEqSrc(attribute) {
+        return attribute.name !== 'src';
+    }
+
+    function pick(attributes, className) {
+        return attributes.find(primary) ||
+            /lazy/.test(className) &&
+            attributes.find(secondary);
+    }
+
+    function primary(attribute) {
+        return /^data(-\w+)*-src$/i.test(attribute.name) &&
+            isSrcValue(attribute.value);
+    }
+
+    function secondary(attribute) {
+        return isSrcValue(attribute.value);
+    }
+
+    function isSrcValue(value) {
+        return isURL(value) || isURL(location.protocol + value);
+    }
+
+    function isURL(value) {
+        try {
+            new URL(value);
+            return true;
+        } catch (TypeError) {
+            return false;
         }
     }
 
