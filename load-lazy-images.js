3fafc0f52daaf06b94b6456a73421a20817805df
Julian Zinn <79761+jzinn@users.noreply.github.com>
2018-05-27 01:17:18 -0700

    lazy: add data-dt-lazy-src

--- load-lazy-images.js
+++ load-lazy-images.js
@@ -16,6 +16,7 @@
     load('data-native-src'); // https://www.bloomberg.com/news/articles/2018-05-14/drone-pilots-2-000-paydays-drop-90-in-race-to-the-bottom
     load('data-original'); // https://www.macrumors.com/
     load('data-src'); // https://www.atlasobscura.com/articles/what-did-pterosaurs-do-with-their-legs
+    load('data-dt-lazy-src'); // https://www.digitaltrends.com/home-theater/how-to-install-kodi-on-an-amazon-fire-tv/
 
     function load(lazy) {
         images().forEach(update);
