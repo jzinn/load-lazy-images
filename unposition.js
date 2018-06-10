7565a39e3be96dd630774bcbb46a2f371c392e80
Julian Zinn <79761+jzinn@users.noreply.github.com>
2018-06-10 01:22:08 -0700

    Add unposition.js

---
+++ unposition.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Unposition
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description
+// @author       You
+// @match        http://*/*
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function(width) {
+	'use strict';
+
+	process(document.documentElement);
+	traverse(document.body);
+
+	function traverse(node) {
+		if (process(node)) return;
+		node.childNodes.forEach(traverse);
+	}
+
+	function process(node) {
+		return !element() || candidate() && restyle(node.style);
+
+		function element() {
+			return node.nodeType === 1;
+		}
+
+		function candidate() {
+			return big() && positioned(getComputedStyle(node));
+		}
+
+		function big() {
+			return node.clientWidth === width;
+		}
+	}
+
+	function positioned(computed) {
+		return computed.position === 'fixed' || computed.position === 'sticky';
+	}
+
+	function restyle(style) {
+		initialize(style, 'position');
+		return true;
+	}
+
+	function initialize(style, property) {
+		style.setProperty(property, 'initial', 'important');
+	}
+})(window.innerWidth);
