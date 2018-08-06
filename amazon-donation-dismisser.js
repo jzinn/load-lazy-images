b7a858def7a210846d5274baa364a57e9df362f7
Julian Zinn <79761+jzinn@users.noreply.github.com>
2018-08-06 10:46:29 -0700

    Add amazon-donation-dismisser.js

---
+++ amazon-donation-dismisser.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Amazon Donation Dismisser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description
+// @author       You
+// @match        https://www.amazon.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	modals()
+		.filter(containing(keywords()))
+		.forEach(dismiss);
+
+	function modals() {
+		return Array.from(document.getElementsByClassName('a-modal-scroller'));
+	}
+
+	function containing(keywords) {
+		return containing_;
+
+		function containing_(modal) {
+			return search(modal.textContent);
+		}
+
+		function search(text) {
+			return keywords.some(match);
+
+			function match(keyword) {
+				return text.contains(keyword);
+			}
+		}
+	}
+
+	function keywords() {
+		return ['charity', 'donate', 'donation', 'smile'];
+	}
+
+	function dismiss(modal) {
+		click(button());
+
+		function button() {
+			return modal.querySelector('button.dismiss');
+		}
+
+		function click(button) {
+			if (!button) return;
+			button.click();
+		}
+	}
+})();
