438cdc5f1ff11758a7ea8c29fb671f2de11762d7
Julian Zinn <79761+jzinn@users.noreply.github.com>
2019-03-25 22:38:22 -0700

    WIP

--- cloudflare-man-in-the-middle-mitigation.js
+++ cloudflare-man-in-the-middle-mitigation.js
@@ -1,3 +1,21 @@
+// From https://usamaejaz.com/cloudflare-email-decoding/
+
+function cfDecodeEmail(encodedString) {
+	var email = '',
+		r = parseInt(encodedString.substr(0, 2), 16),
+		n,
+		i;
+	for (n = 2; encodedString.length - n; n += 2) {
+		i = parseInt(encodedString.substr(n, 2), 16) ^ r;
+		email += String.fromCharCode(i);
+	}
+	return email;
+}
+
+console.log(cfDecodeEmail('543931142127353935313e352e7a373b39'));
+
+/////
+
 !(function() {
 	'use strict';
 	function e(e) {
